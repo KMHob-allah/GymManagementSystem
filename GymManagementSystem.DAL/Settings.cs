bb313029ee041d88859b1a00e81084981e0ed36c
omar mohame <km00550055@gmail.com>
2026-09-20 21:06:33 -0700

    Set up DAL configuration

---
+++ GymManagementSystem.DAL/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GymManagementSystem.DAL
+{
+    static public class Settings
+    {
+    
+        public static string ConnectionString => 
+            ConfigurationManager.ConnectionStrings["GymManagementSystem"].ConnectionString;
+    }
+}
+    
